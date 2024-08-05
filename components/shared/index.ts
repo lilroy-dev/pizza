cabb760e69b8dfa33aebd2acb41c15494798114a
sv.konoplitskii <s.konoplitsky@yandex.com>
2024-08-05 21:43:08 +0300

    @ "Добавил блок фильтров"

---
+++ components/shared/index.ts
@@ -0,0 +1,10 @@
+export  { Container } from  './container';
+export  { Header } from  './header';
+export  { Title } from  './title';
+export  { SortPopup } from  './sort-popup';
+export  { TopBar } from  './top-bar';
+export  { Categories } from  './categories';
+export  { Filters } from  './filters';
+export  { FilterCheckbox } from  './filter-checkbox';
+export  { RangeSlider } from  './range-slider';
+export  { CheckboxFiltersGroup } from  './checkbox-filters-group';
